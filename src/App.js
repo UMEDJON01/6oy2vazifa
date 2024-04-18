065320f93be39bb5e94bf6e33e8808ab070f69ab
umedjon01 <najottalimfn15@gmail.com>
2024-04-18 07:33:32 +0500

    kod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "../src/index.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const images = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg",
+    "image5.jpg",
+    "image6.jpg",
+    "image7.jpg",
+    "image8.jpg",
+    "image9.jpg",
+    "image10.jpg",
+  ];
+  const music = new Audio("./assets/music.mp3");
+
+  const handleNext = () => {
+    if (counter < images.length - 1) {
+      setCounter(counter + 1);
+      music.currentTime = 0;
+      music.play();
+    }
+  };
+
+  const handleBack = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+      music.currentTime = 0;
+      music.play();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <img src={`./assets/${images[counter]}`} alt="RASM" className="image" />
+      <div className="button-container">
+        <button
+          className={`button ${
+            counter === images.length - 1 ? "highlight1" : ""
+          }`}
+          onClick={handleBack}
         >
-          Learn React
-        </a>
-      </header>
+          BACK
+        </button>
+        <button
+          className={`button ${
+            counter === images.length - 1 ? "highlight" : ""
+          }`}
+          onClick={handleNext}
+        >
+          NEXT
+        </button>
+      </div>
     </div>
   );
 }
